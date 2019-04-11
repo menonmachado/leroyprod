5426a8f3c8b8d3f56afe797f2d51f3ccd154ba6b
Jean Farrell <jeanfarrell@gmail.com>
2019-04-11 08:36:05 -0300

    Creating routes on routes/api.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,24 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+Route::get('product', function() {
+    // If the Content-Type and Accept headers are set to 'application/json', 
+    // this will return a JSON structure. This will be cleaned up later.
+    return Product::all();
 });
+
+
+Route::post('product', function(Request $request) {
+    return Product::insertOrUpdate($request->all);
+});
+
+
+Route::delete('product', function() {
+    Product::delete();
+    return 204;
+})
