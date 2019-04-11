5f20e1329d367b698a7ad53351600ff4c3ee4e78
Jean Farrell <jeanfarrell@gmail.com>
2019-04-11 07:56:09 -0300

    Creating product table migration.

---
+++ database/migrations/2019_04_11_102243_create_product_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('lm');
+            $table->string('name');
+            $table->integer('free_shipping');
+            $table->string('description');
+            $table->decimal('price');
+            $table->integer('category_lm');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
